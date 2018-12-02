99ee8daf1110d72f00c23c336aa307594d73469e
whoustian <whoustian@gmail.com>
2018-12-02 11:21:39 -0500

    added selenium

--- src/main/java/webCrawlerPackage/SpiderTest.java
+++ src/main/java/webCrawlerPackage/SpiderTest.java
@@ -5,8 +5,8 @@ public class SpiderTest {
 	public static void main(String[] args) {
 
 		Spider spider = new Spider();
-		spider.search("http://amazon.com/", "VR");
-
+		spider.search("http://amazon.com/", "Echo");
+		
 	}
 
 }
