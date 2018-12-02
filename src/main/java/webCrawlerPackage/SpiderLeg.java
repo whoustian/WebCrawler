9b0f2b078f4fa773754035599e34bdcb4a46f288
whoustian <whoustian@gmail.com>
2018-12-02 10:48:22 -0500

    Initial commit

---
+++ src/main/java/webCrawlerPackage/SpiderLeg.java
@@ -0,0 +1,46 @@
+package webCrawlerPackage;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SpiderLeg {
+
+	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+	private List<String> links = new LinkedList<String>();
+	private Document htmlDocument;
+
+	public void crawl(String url) {
+		try {
+			Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+			Document htmlDocument = connection.get();
+			this.htmlDocument = htmlDocument;
+
+			System.out.println("Received web page at " + url);
+
+			Elements linksOnPage = htmlDocument.select("a[href]");
+			System.out.println("Found (" + linksOnPage.size() + ") links");
+			for (Element link : linksOnPage) {
+				this.links.add(link.absUrl("href"));
+			}
+		} catch (IOException ioe) {
+			System.out.println("Error in out HTTP request " + ioe);
+		}
+	}
+
+	public boolean searchForWord(String searchWord) {
+		System.out.println("Searching for the word " + searchWord + "...");
+		String bodyText = this.htmlDocument.body().text();
+		return bodyText.toLowerCase().contains(searchWord.toLowerCase());
+	}
+
+	public List<String> getLinks() {
+		return this.links;
+	}
+}
---
+++ src/main/java/webCrawlerPackage/SpiderTest.java
@@ -0,0 +1,12 @@
+package webCrawlerPackage;
+
+public class SpiderTest {
+
+	public static void main(String[] args) {
+
+		Spider spider = new Spider();
+		spider.search("http://amazon.com/", "Oculus");
+
+	}
+
+}
